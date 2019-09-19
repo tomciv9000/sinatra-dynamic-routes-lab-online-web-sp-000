eb091a3b8d89ea90a4ce95e8ba35d6667638462f
Tom White <eugene.white@gmail.com>
2019-09-19 04:46:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
   get "/reversename/:name" do
     @reversed_name = params[:name].reverse!
-    "#{@user_name}"
+    "#{@reversed_name}"
   end
   
 end

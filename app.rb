ff3e18586d02cd91995d4dffe4bc0ae3f2aec017
Tom White <eugene.white@gmail.com>
2019-09-19 05:11:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,7 +29,7 @@ class App < Sinatra::Base
     if @operation = "add"
       "#{@num1 + @num2}"
       elsif @operation = "subtract"
-        "#{@num2 - @num1}"  
+        "#{@num1 - @num2}"  
       elsif @operation = "multiply"
       "#{@num1*@num2}" 
       elsif @operation = "divide"

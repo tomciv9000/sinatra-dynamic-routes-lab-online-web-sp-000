20c70aefae489d1f974f3b771f67daaa7c4448cc
Tom White <eugene.white@gmail.com>
2019-09-19 04:43:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+  
 end

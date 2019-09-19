068cbba91539386f3cdc12d2365070b9a37bfc88
Tom White <eugene.white@gmail.com>
2019-09-19 04:44:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get "/goodbye/:name" do
+  get "/reversename/:name" do
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end
